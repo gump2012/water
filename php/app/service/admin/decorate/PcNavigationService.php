d346217d3f8bf69565f8264652e77894b77a2796
gump_1_85150091 <85150091@qq.com>
2026-08-06 12:50:02 +0800

    其他中增加订水

--- php/app/service/admin/decorate/PcNavigationService.php
+++ php/app/service/admin/decorate/PcNavigationService.php
@@ -244,6 +244,7 @@ class PcNavigationService extends BaseService
             ["name" => "站内消息", "app_link" => '', "link" => "member/userMessage/list"],
             ["name" => "发票管理", "app_link" => '', "link" => "member/orderInvoice/list"],
             ["name" => "账号信息", "app_link" => '', "link" => "member/profile/info"],
+            ["name" => "订水", "app_link" => '', "link" => "/pages/water/index"],
 
         ];
         if (config('app.IS_MERCHANT')) {
